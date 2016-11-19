ecb51cdba6e18adcf13de83d0fba87679bb627e7
Tyler Waters <tyler.waters@gmail.com>
2016-11-19 14:14:42 -0800

    Adding debugger to the watch command

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -6,10 +6,10 @@ import {Provider} from 'react-redux'
 import {Router, browserHistory} from 'react-router'
 import {syncHistoryWithStore} from 'react-router-redux'
 
-import configueStore from '../common/store'
+import configureStore from '../common/store'
 import routes from '../common/routes.jsx'
 
-const store = configueStore(browserHistory, window.LOCALS)
+const store = configureStore(browserHistory, window.LOCALS)
 const history = syncHistoryWithStore(browserHistory, store)
 
 render(
