978913384e77cc8584f434b942dc81fe68cc9eb4
Tyler Waters <tyler.waters@gmail.com>
2017-03-12 22:27:59 -0700

    Updating unauthorized to use common error

--- src/server/lib/middleware/authentication.js
+++ src/server/lib/middleware/authentication.js
@@ -1,15 +1,14 @@
 
 import {UserModel} from 'server/lib/models'
+import {Unauthorized} from 'server/lib/errors'
 
 const userModel = new UserModel()
 
 export default (required = true) => (req, res, next) => {
   const token = req.session.token || req.headers['x-token']
-  const unauthorized = new Error('unauthorized')
 
-  unauthorized.status = 401
   if (!token && required) {
-    return next(unauthorized)
+    return next(new Unauthorized('unauthorized'))
   } else if (!token) {
     return next()
   }
