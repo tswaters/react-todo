61c133874ecf366f3d18345d19839fc63f13edbe
Tyler Waters <tyler.waters@gmail.com>
2017-04-15 20:30:25 -0700

    Forgot Error.jsx

---
+++ src/common/pages/Error.jsx
@@ -0,0 +1,36 @@
+
+import React, {PureComponent, PropTypes} from 'react'
+import {connect} from 'react-redux'
+import {FormattedMessage} from 'react-intl'
+
+import {getRequestStats} from 'common/redux/api'
+
+@connect(state => getRequestStats(state))
+class ErrorPage extends PureComponent {
+
+  static defaultProps = {
+    requestError: null
+  }
+
+  static propTypes = {
+    requestError: PropTypes.object
+  }
+
+  render () {
+    const err = this.props.requestError
+      ? (
+        <pre>
+          {this.props.requestError.message}
+          {this.props.requestError.stack}
+        </pre>
+      ) : null
+    return (
+      <h2>
+        <FormattedMessage id="500" />
+        {err}
+      </h2>
+    )
+  }
+}
+
+export default ErrorPage
