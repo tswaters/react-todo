0b9a4040d840e65eacbf07533b3cfa4d8744a61b
Tyler Waters <tyler.waters@gmail.com>
2017-04-29 16:27:27 -0700

    Proper fix to .map files showing up as 404

--- webpack/loaders.js
+++ webpack/loaders.js
@@ -1,5 +1,6 @@
 
 module.exports = [
   {test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
+  {test: /\.js$/, include: /node_modules/, loaders: ['strip-sourcemap-loader']},
   {test: /\.json$/, loader: 'json-loader'}
 ]
