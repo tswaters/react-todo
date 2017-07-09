1c7f63577bbd02b85e862d737658d227a4615c65
Tyler Waters <tyler.waters@gmail.com>
2017-07-09 14:55:42 -0700

    Missed a few things required for webpack changes

--- src/server/app.js
+++ src/server/app.js
@@ -20,7 +20,7 @@ const app = express()
 
 app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static(join(__dirname, '../public')))
+app.use(express.static(join(__dirname, '../client')))
 app.use(json())
 app.use(session({
   store: new SessionStore({
