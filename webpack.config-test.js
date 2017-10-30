9e1d68ed9c5f53002e809ac92ca8314d62ae78af
Tyler Waters <tyler.waters@gmail.com>
2017-10-29 17:30:14 -0700

    Fix up webpack.test.js

--- webpack.config-test.js
+++ webpack.config-test.js
@@ -3,13 +3,10 @@ const config = require('./webpack')
 
 const testConfig = config(true)
 
-testConfig.output = {
-  devtoolModuleFilenameTemplate: '[absolute-resource-path]',
-  devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
-}
+testConfig.output.devtoolModuleFilenameTemplate = '[absolute-resource-path]'
+testConfig.output.devtoolFallbackModuleFilenameTemplate = '[absolute-resource-path]?[hash]'
 
 testConfig.devtool = 'cheap-module-source-map'
 
-delete testConfig.entry
 
 module.exports = testConfig
