5154f34200cbd26c8101362afd07a7b624e6fcdd
Tyler Waters <tyler.waters@gmail.com>
2017-12-17 18:09:10 -0800

    More tests for auth controller

--- src/test/unit/server/api/auth.test.js
+++ src/test/unit/server/api/auth.test.js
@@ -16,12 +16,26 @@ describe('auth controller', () => {
   const register = sinon.stub()
   const login = sinon.stub()
   const logout = sinon.stub()
+  const changePassword = sinon.stub()
+  const update = sinon.stub()
 
   before(done => {
     const authController = proxyquire('server/api/auth', {
-      'server/models/user': {default: {register, login, logout}}
+      'server/middleware/authorization': {default: () => (req, res, next) => next()},
+      'server/middleware/authentication': {default: () => (req, res, next) => next()},
+      'server/models/user': {default: {
+        register,
+        login,
+        logout,
+        changePassword,
+        update
+      }}
     }).default
     const {app, context} = appFactory()
+    context.use((req, res, next) => {
+      res.locals.user = {id: '1234', userName: 'userName'}
+      next()
+    })
     context.use('/api/auth', authController)
     client = supertest(app)
     server = app.listen(PORT, done)
@@ -31,13 +45,100 @@ describe('auth controller', () => {
     register.reset()
     login.reset()
     logout.reset()
+    changePassword.reset()
+    update.reset()
   })
 
   after(done => {
     server.close(done)
   })
 
-  describe('password', () => {
+  describe('POST /password', () => {
+
+    let payload = null
+
+    beforeEach(() => {
+      payload = {userName: 'userName', oldPassword: 'oldPassword', newPassword: 'newPassword'}
+    })
+
+    it('should handle errors', async () => {
+      changePassword.rejects({status: 500, message: 'aw snap!'})
+
+      await client.post('/api/auth/password')
+        .send(payload)
+        .expect(500, {status: 500, message: 'aw snap!'})
+
+      assert.equal(changePassword.callCount, 1)
+      const [, {userName, oldPassword, newPassword}] = changePassword.firstCall.args
+      assert.equal(userName, 'userName')
+      assert.equal(oldPassword, 'oldPassword')
+      assert.equal(newPassword, 'newPassword')
+
+    })
+
+    it('should return the result', async () => {
+
+      changePassword.resolves({userName: 'userName'})
+
+      await client.post('/api/auth/password')
+        .send(payload)
+        .expect(200, {userName: 'userName'})
+
+      assert.equal(changePassword.callCount, 1)
+      const [, {userName, oldPassword, newPassword}] = changePassword.firstCall.args
+      assert.equal(userName, 'userName')
+      assert.equal(oldPassword, 'oldPassword')
+      assert.equal(newPassword, 'newPassword')
+
+    })
+
+  })
+
+  describe('POST /profile', () => {
+
+    let payload = null
+
+    beforeEach(() => {
+      payload = {userName: 'userName'}
+    })
+
+    it('should handle errors', async () => {
+      update.rejects({status: 500, message: 'aw snap!'})
+
+      await client.post('/api/auth/profile')
+        .send(payload)
+        .expect(500, {status: 500, message: 'aw snap!'})
+
+      assert.equal(update.callCount, 1)
+      assert.deepEqual(update.firstCall.args[0], {userName: 'userName'})
+      assert.deepEqual(update.firstCall.args[1], {where: {id: '1234'}})
+
+    })
+
+    it('should return the result', async () => {
+
+      changePassword.resolves({userName: 'userName'})
+
+      await client.post('/api/auth/profile')
+        .send(payload)
+        .expect(200, {id: '1234', userName: 'userName'})
+
+      assert.equal(update.callCount, 1)
+      assert.deepEqual(update.firstCall.args[0], {userName: 'userName'})
+      assert.deepEqual(update.firstCall.args[1], {where: {id: '1234'}})
+
+    })
+
+  })
+
+  describe('GET /profile', () => {
+
+    it('should return the result', async () => {
+
+      await client.get('/api/auth/profile')
+        .expect(200, {userName: 'userName'})
+
+    })
 
   })
 
